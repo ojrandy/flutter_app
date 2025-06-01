80910183f1e80127b4015375f55df28715a1a033
Ojong Randy Ikoke <solomon.peters676@gmail.com>
2025-06-01 12:42:31 +0100

    Second day with Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -8,31 +8,41 @@ void main() {
           title: Text('Barbers Shop', style: TextStyle(fontSize: 20)),
           backgroundColor: Colors.blueAccent,
         ),
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              // Define a linear gradient with three colors
-              colors: [
-                Colors.blue,
-                const Color.fromARGB(193, 11, 7, 219),
-                Color.fromARGB(255, 5, 182, 129),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Welcome to the Barbers',
-              style: TextStyle(
-                fontSize: 24,
-                color: Color.fromARGB(255, 255, 255, 255),
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+// Creates a class to store the container Widgets
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          // Define a linear gradient with three colors
+          colors: [
+            Colors.blue,
+            const Color.fromARGB(193, 11, 7, 219),
+            Color.fromARGB(255, 5, 182, 129),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Welcome to the Barbers booking center',
+          style: TextStyle(
+            fontSize: 22,
+            color: Color.fromARGB(255, 255, 255, 255),
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+    );
+  }
 }
