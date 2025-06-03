aa1adafb71ef5ed2baf351f96b41f162a999dde0
Ojong Randy Ikoke <solomon.peters676@gmail.com>
2025-06-03 16:42:02 +0100

    initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Barbers Shop', style: TextStyle(fontSize: 12)),
+          backgroundColor: Colors.blueAccent,
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              // Define a linear gradient with two colors
+              colors: [
+                Colors.blue,
+                const Color.fromARGB(117, 231, 179, 175),
+                Color.fromARGB(255, 3, 139, 202),
+              ],
+              begin: Alignment.centerLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Welcome to the Barbers Shop',
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.black,
+                fontWeight: FontWeight.w800,
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
